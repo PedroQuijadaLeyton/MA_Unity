93e0dd80e0d97ac6e3a2464c3367ae4792e3f6af
PedroQuijadaLeyton <pedro@quiley.com>
2017-11-17 13:56:56 +0000

    Added animation to the door

---
+++ Assets/Script/Door_handle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_handle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("START DOOR");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            GetComponent<Animator>().SetBool("open", true);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            GetComponent<Animator>().SetBool("open", false);
+    }
+}
